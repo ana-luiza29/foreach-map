1f0d89b9b7e59548ce52a91d831013536708abcb
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:02:07 -0400

    finalizada questao

---
+++ questao20.js
@@ -0,0 +1,8 @@
+/*   
+Questão 20 - Qual é a utilidade de forEach() se ele não retornar nenhum valor?  
+*/
+
+/*
+resposta
+ sua capacidade de realizar uma ação para cada elemento do array, como executar uma função em cada elemento ou modificar o estado fora do loop, mesmo sem retornar valores.
+ */
