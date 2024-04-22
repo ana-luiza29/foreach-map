a7c8492e04495112e4f180d9eef9d8bcbe49ad50
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:01:25 -0400

    finalizada questao

---
+++ questao13.js
@@ -0,0 +1,7 @@
+/* 
+Questão 13 - Quando você usaria map() em vez de forEach()?  
+*/
+
+
+/*resposta 
+map() é ideal quando você precisa de uma nova matriz com os valores modificados, enquanto forEach() é mais apropriado quando você precisa apenas iterar sobre os elementos sem retornar uma nova */
