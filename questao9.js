e46a22c192e9a0bc94dc79ff2ef06e082a28943b
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:00:57 -0400

    finalizada questao

---
+++ questao9.js
@@ -0,0 +1,9 @@
+/*   
+
+Questão 09 - Qual é a diferença fundamental entre map() e forEach()?  
+
+ 
+
+resposta
+forEach ele percorre o array e ele tem a capacidade de modificar o array e ele retornar ao array com as mesma modifcaçoes, ja o map vai criar um array e ele vai modificar com o mesmo elemento que esta passando
+*/
