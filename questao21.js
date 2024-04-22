3b5aefe538c6c9adc3bed01fd86cf0e8a2ed9567
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:02:11 -0400

    finalizada questao

---
+++ questao21.js
@@ -0,0 +1,10 @@
+/*   
+Questão 21 - Existe alguma diferença de desempenho entre map() e 
+forEach()?  
+
+*/
+
+/*
+resposta 
+ forEach e map têm desempenho semelhante, pois ambos percorrem todos os elementos do array. A diferença de desempenho entre eles seria negligível na maioria dos casos.
+ */
