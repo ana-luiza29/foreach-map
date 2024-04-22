86d8f844f913915063d289052e9a4f18765a7cde
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:01:34 -0400

    finalizada questao

---
+++ questao15.js
@@ -0,0 +1,8 @@
+/*   
+Questão 15 - Qual é o propósito principal do método map()?  
+*/
+
+/*
+resposta
+é transformar cada elemento de um array e retornar um novo array com os resultados da transformação, mantendo o mesmo tamanho do array original.
+*/
