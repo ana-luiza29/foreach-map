55d4d2b40699b258bd8fdc17f9cd4830476356de
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-22 11:00:05 -0400

    finalizada questao

---
+++ questao2.js
@@ -0,0 +1,13 @@
+/*
+ 
+Questão 02 - Crie um algoritmo usando o método map() para converter um 
+array de strings para um array de números?  
+ 
+
+*/
+resposta
+const arrayDeStrings = ["1", "2", "3", "4", "5"];
+
+const arrayDeNumeros = arrayDeStrings.map(Number);
+
+console.log(arrayDeNumeros);
